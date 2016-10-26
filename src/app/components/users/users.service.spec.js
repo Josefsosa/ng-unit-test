14debd36cb7d4660a6fb6e606cef39e5ba1f0eb8
Jose F. Sosa <josefsosa@gmail.com>
2016-10-25 23:52:13 -0700

    Added more test

--- src/app/components/users/users.service.spec.js
+++ src/app/components/users/users.service.spec.js
@@ -5,16 +5,35 @@
    * @todo Complete the test
    * This example is test the user service.
    */
-  describe('service users', function() {
+  describe('User Injector', function() {
+    //Inject a specific ng module
+    beforeEach(module('ngResource', function($provide) {
+      $provide.value('fooVal', 42);
+    }));
+
+    it('should have ngResource defined', inject(function ($resource, fooVal) {
+      expect($resource).toBeDefined;
+      expect(fooVal).toBe(42);
+    }));
+
+    describe('service users', function() {
+      //Inject a specific ng module
+      beforeEach(module('ngResource'));
+
+      it('should have ngResource defined', inject(function ($resource) {
+        expect($resource).toBeDefined;
+      }));
+    });
+
     it('should have test', function () {
       expect(true).toBe(true);
     });
-  });
-
-  describe('Users factory', function() {
-    it('has a dummy spec to test 2 + 2', function() {
-      expect(2 + 2).toEqual(4);
     });
-  });
+
+    describe('Users factory', function() {
+      it('has a dummy spec to test 2 + 2', function() {
+        expect(2 + 2).toEqual(4);
+      });
+    });
 
 })();
